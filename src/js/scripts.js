acfcdd2c2eb4ae928bc51cd389eaca219a3628f1
CTC3PO <chuinus@gmail.com>
2023-06-05 00:16:45 -0400

    first commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,9 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader.js';
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -12,13 +12,13 @@ document.body.appendChild(renderer.domElement);
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    45,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  45,
+  window.innerWidth / window.innerHeight,
+  0.5,
+  3000
 );
 
-renderer.setClearColor(0xA3A3A3);
+renderer.setClearColor(0xa3a3a3);
 
 const orbit = new OrbitControls(camera, renderer.domElement);
 
@@ -43,29 +43,29 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 4;
 
-let car;
+let object;
 
-rgbeLoader.load('./assets/MR_INT-005_WhiteNeons_NAD.hdr', function(texture) {
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = texture;
+rgbeLoader.load("./assets/MR_INT-005_WhiteNeons_NAD.hdr", function (texture) {
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  scene.environment = texture;
 
-    gltfLoader.load('./assets/scene.gltf', function(gltf) {
-        const model = gltf.scene;
-        scene.add(model);
-        car = model;
-    });
+  gltfLoader.load("./assets/london/scene.gltf", function (gltf) {
+    const model = gltf.scene;
+    scene.add(model);
+    object = model;
+  });
 });
 
 function animate(time) {
-    if(car)
-        car.rotation.y = - time / 3000;
-    renderer.render(scene, camera);
+  //uncomment this to enable rotatin of object
+  //   if (object) object.rotation.y = -time / 3000;
+  renderer.render(scene, camera);
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
 });
